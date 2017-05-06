b631874c28e62b0a9dbbcbe7358fd857155fbf6e
janestar <janestar92@163.com>
2017-05-06 13:32:09 +0800

    fix bargain

--- miniProjectBackend/miniDevApp/views.py
+++ miniProjectBackend/miniDevApp/views.py
@@ -1,12 +1,12 @@
 # -*-coding:utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
-import simplejson
+#import simplejson
+import json
 from miniDevApp.models import BottleInfo
 from miniDevApp.models import WishList
 
 # Create your views here.
-
 def getItemList(request):
     type = request.GET['type']
     if type == '1':
@@ -14,7 +14,7 @@ def getItemList(request):
         bottle_list = []
         for bottle in bottles:
             bottle_list.append(bottle.getDict())
-        return HttpResponse(simplejson.dumps({"itemList":bottle_list}, ensure_ascii=False))        
+        return HttpResponse(json.dumps({"itemList":bottle_list}, ensure_ascii=False))        
     if type == '2':
         wishes = WishList.objects.filter(wishUserInfo="427290210")
         bottle_list = []
@@ -23,7 +23,7 @@ def getItemList(request):
             bottle_info = bottle.getDict()
             bottle_info["itemStatus"] = wish.bottleStatus
             bottle_list.append(bottle_info)
-        return HttpResponse(simplejson.dumps({"itemList":bottle_list}, ensure_ascii=False))
+        return HttpResponse(json.dumps({"itemList":bottle_list}, ensure_ascii=False))
 
 
 def getItemDetail(request):
@@ -32,21 +32,25 @@ def getItemDetail(request):
     bottle_list = []
     for bottle in bottle_info:
         bottle_list.append(bottle.getDict())
-    return HttpResponse(simplejson.dumps(bottle_list, ensure_ascii=False))
-
+    return HttpResponse(json.dumps(bottle_list, ensure_ascii=False))
 
 
 
 def confirmBargain(request):
     itemId = request.POST['itemId']
     bottle_info = BottleInfo.objects.filter(bottleId=itemId)
-    bottle_info.bottleStatus = 1
-    bottle_info.save()
+    for bottle in bottle_info:
+        bottle.bottleStatus = 1
+        bottle.save()
+    #print bottle_info[0].bottleStatus
+    #bottle_info[0].bottleStatus = 1
+    #bottle_info[0].save()
+    #print bottle_info[0].bottleStatus
     #DICT = {‘ret’:'0', 'ret_msg':'确认成功'}
     bargainStatus = dict()
     bargainStatus['ret']='0'
-    bargainStatus['ret_msg']='确认成功'
-    return HttpResponse(simplejson.dumps(bargainStatus, ensure_ascii=False))
+    bargainStatus['ret_msg']='你好'
+    return HttpResponse(json.dumps(bargainStatus, ensure_ascii=False))
 def cancelBargain(request):
     itemId = request.POST['itemId']
     bottle_info = BottleInfo.objects.filter(bottleId=itemId)
@@ -55,5 +59,5 @@ def cancelBargain(request):
    # DICT = {‘ret’:'0', 'ret_msg':'删除成功'}
     bargainStatus = dict()
     bargainStatus['ret']='0'
-    bargainStatus['ret_msg']='删除成功'
-    return HttpResponse(simplejson.dumps(bargainStatus, ensure_ascii=False))
+    bargainStatus['ret_msg']='ok'
+    return HttpResponse(json.dumps(bargainStatus, ensure_ascii=False))
--- miniProjectBackend/miniProjectBackend/settings.py
+++ miniProjectBackend/miniProjectBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l0#3v7-^raickx%oi*@2ab7uy7j+3oxh0$ljvl@pjfqc74fo-r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -78,10 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "minidb",
-        'USER': 'root',
-        'PASSWORD':'root',
-        'HOST':'127.0.0.1',
-        'PORT':'3306'
+        'USER': 'cdb_outerroot',
+        'PASSWORD':'root@123',
+        'HOST':'590d361234f24.gz.cdb.myqcloud.com',
+        'PORT':'6248'
     }
 }
 
