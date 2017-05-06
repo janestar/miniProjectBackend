2f7fe1c65db6982b50d7c6c20a95ddbde58c2cb0
jaylonliu(刘良建) <jaylonliu@tencent.com>
2017-05-06 10:02:40 +0800

    fix bargain cacel

--- miniProjectBackend/miniDevApp/views.py
+++ miniProjectBackend/miniDevApp/views.py
@@ -24,17 +24,27 @@ def getItemDetail(request):
     for bottle in bottle_info:
         bottle_list.append(bottle.getDict())
     return HttpResponse(simplejson.dumps(bottle_list, ensure_ascii=False))
+
+
+
+
 def confirmBargain(request):
     itemId = request.POST['itemId']
     bottle_info = BottleInfo.objects.filter(bottleId=itemId)
     bottle_info.bottleStatus = 1
     bottle_info.save()
-    DICT = {‘ret’:'0', 'ret_msg':'确认成功'}
-    return HttpResponse(simplejson.dumps(DICT, ensure_ascii=False))
+    #DICT = {‘ret’:'0', 'ret_msg':'确认成功'}
+    bargainStatus = dict()
+    bargainStatus['ret']='0'
+    bargainStatus['ret_msg']='确认成功'
+    return HttpResponse(simplejson.dumps(bargainStatus, ensure_ascii=False))
 def cancelBargain(request):
     itemId = request.POST['itemId']
     bottle_info = BottleInfo.objects.filter(bottleId=itemId)
     bottle_info.bottleStatus = -1
     bottle_info.save()
-    DICT = {‘ret’:'0', 'ret_msg':'删除成功'}
-    return HttpResponse(simplejson.dumps(DICT, ensure_ascii=False))
+   # DICT = {‘ret’:'0', 'ret_msg':'删除成功'}
+    bargainStatus = dict()
+    bargainStatus['ret']='0'
+    bargainStatus['ret_msg']='删除成功'
+    return HttpResponse(simplejson.dumps(bargainStatus, ensure_ascii=False))
